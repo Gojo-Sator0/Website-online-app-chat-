2f372c9bc8a13c04b39e3e6df0a61f2a9e363cea
Gojo Satoro <vladimirkul06@gmail.com>
2025-01-31 22:41:56 +0200

    Add postgresql

--- app/base/settings.py
+++ app/base/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chat',
+        'USER': 'chat',
+        'PASSWORD': '893520',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
