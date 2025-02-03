334713c61851939a9c595f266ab5094d4019dac2
Gojo Satoro <vladimirkul06@gmail.com>
2025-02-03 13:35:48 +0200

    Button logout

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
+from django.contrib.auth.views import LogoutView
 from accounts import views
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view, name='profile'),
-
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
 ]
