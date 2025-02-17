9f0d473b6125aa411498a6c40e8b5923d75312aa
Gojo Satoro <vladimirkul06@gmail.com>
2025-02-17 14:28:19 +0200

    Change url profile

--- app/accounts/urls.py
+++ app/accounts/urls.py
@@ -6,7 +6,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
-    path('profile/', views.profile_view, name='profile'),
+    path('profile/<str:username>', views.profile_view, name='profile'),
     path('logout/', views.logout_view, name='logout'),
 ]
 
--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from accounts.models import User
 # Create your views here.
 
 def login_view(request):
@@ -41,19 +42,26 @@ def register_view(request):
     return render(request, 'accounts/register.html', context)
 
 @login_required
-def profile_view(request):
-    if request.method == 'POST':
-        form = UserProfileForm(data=request.POST, instance=request.user, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('accounts:profile'))
+def profile_view(request, username):
+    profile_user = get_object_or_404(User, username=username)
+
+    if request.user == profile_user:
+        if request.method == 'POST':
+            form = UserProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect(reverse('user_profile', args=[request.user.username]))
+        else:
+            form = UserProfileForm(instance=request.user)
     else:
-        form = UserProfileForm(instance=request.user)
-        
+        form = None  # Другие пользователи не могут редактировать чужие профили
+
     context = {
-        'form': form
+        'profile_user': profile_user,
+        'form': form,
     }
     return render(request, 'accounts/profile.html', context)
+
 
 @login_required
 def logout_view(request):
